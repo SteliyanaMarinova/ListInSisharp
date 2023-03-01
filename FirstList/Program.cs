50491031816a40e13a3bf20f5318fb71b1de23c6
SteliyanaMarinova <stelimarinova2007@abv.bg>
2023-03-01 11:16:52 +0200

    Add project files.

---
+++ FirstList/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vuvedi broi elementi: ");
+            int n = int.Parse(Console.ReadLine());
+            List<int> numbers = new List<int>();
+
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Vuvedi chislo: ");
+                int item = int.Parse(Console.ReadLine());
+                numbers.Add(item);
+            }
+
+            //int newIndex = n / 2;
+            //numbers.Insert(newIndex, 6);
+
+            //int minElement = numbers.Min();
+            //numbers.Add(minElement);
+
+            //foreach (var chervenoKvadratche in numbers)
+            while (numbers.Contains(0))
+            {
+                numbers.Remove(0);
+            }
+            //{
+            //    if (chervenoKvadratche == 0)
+            //    {
+            //        numbers.Remove(chervenoKvadratche);
+            //    }
+            //  //numbers.Remove(0);
+
+            //}
+            
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.WriteLine($"numbers[{i}] = {numbers[i]}");
+            }
+            Console.WriteLine("Broqt na elementite v spisuka e: " + numbers.Count);
+        }
+    }
+}
---
+++ ListFibonachi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListFibonachi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+  
+            List<ulong> numbers = new List<ulong>();
+
+            numbers.Add(1);
+            numbers.Add(1);  
+            Console.WriteLine();
+            Console.WriteLine("Vuvedi do koe chislo na Fibonachi iskash da vkarash: ");
+            byte numFib = byte.Parse(Console.ReadLine());
+            for (int i = 2; i < numFib; i++)
+            {
+                numbers.Add(numbers[i-2] + numbers[i-1]);
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
